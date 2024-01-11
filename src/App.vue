1853426fe12fbdc864ca7e22f96160e4590a5df2
0xa1d1 <0xa1d1+git@pm.me>
2024-01-11 12:32:53 +0700

    vue conversion

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img src="./assets/logo.png" style="width:200px;margin:auto" />
+  <p>venture@000.capital <a href="https://twitter.com/000_Capital" target="_blank">@000_Capital</a></p>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+html,
+body {
+    margin: 0;
+    padding: 0;
+    background: black;
+    text-align: center;
+    padding-top: 100px;
+    color: #eee;
+    font-family: 'Courier New', Courier, monospace;
+}
+p{
+    line-height: 2;
+}
+a{
+    color:inherit;
+    display:block;
+    text-decoration: none;
+}
+</style>
