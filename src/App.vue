c830e67c06de4d1755d9feb5dfc4ecf809216c60
0xa1d1 <0xa1d1+git@pm.me>
2024-01-21 13:18:13 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@
     <div class="section home section-with-link" id="home">
       <div class="section-inner container px-4">
         <div class="text-center px-4 px-md-0">
-          <svg class="img-fluid" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="594.5" height="322.07"
-            viewBox="0 0 594.5 322.07">
+          <svg class="img-fluid " xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="594.5" height="322.07" viewBox="0 0 594.5 322.07">
             <path
               d="M525.348,322.07V252.823h11.526v57.626H594.5V322.07Zm-41.873,0V298.348h-46V322.07H425.946V265.887a12.881,12.881,0,0,1,12.966-12.966H482.13a12.5,12.5,0,0,1,6.482,1.729,13.224,13.224,0,0,1,4.707,4.706,12.5,12.5,0,0,1,1.776,6.531V322.07ZM438,265.022a1.6,1.6,0,0,0-.528,1.249v20.551h46V266.271a1.6,1.6,0,0,0-.528-1.249,1.853,1.853,0,0,0-1.3-.48H439.2A1.726,1.726,0,0,0,438,265.022ZM359.006,322.07V264.542H330.193V252.921h69.148v11.621H370.627V322.07Zm-66.943,0V252.921h11.43V322.07Zm-94.409,0V253.017H253.84a12.261,12.261,0,0,1,6.482,1.776,13.8,13.8,0,0,1,4.706,4.706,12.27,12.27,0,0,1,1.777,6.483V285a12.362,12.362,0,0,1-1.777,6.435,13.262,13.262,0,0,1-4.706,4.706,12.515,12.515,0,0,1-6.482,1.729l-44.661.1V322.07Zm12.054-57a1.664,1.664,0,0,0-.528,1.2v18.247a1.617,1.617,0,0,0,.528,1.249,1.732,1.732,0,0,0,1.2.48H253.36a1.852,1.852,0,0,0,1.3-.48,1.61,1.61,0,0,0,.527-1.249V266.271a1.655,1.655,0,0,0-.527-1.2,1.76,1.76,0,0,0-1.3-.528H210.908A1.652,1.652,0,0,0,209.707,265.07Zm-53.829,57V298.348h-46V322.07H98.347V265.887a12.881,12.881,0,0,1,12.966-12.966H154.53a12.515,12.515,0,0,1,6.485,1.729,13.213,13.213,0,0,1,4.7,4.706,12.493,12.493,0,0,1,1.78,6.531V322.07ZM110.4,265.022a1.6,1.6,0,0,0-.526,1.249v20.551h46V266.271a1.6,1.6,0,0,0-.531-1.249,1.843,1.843,0,0,0-1.3-.48H111.6A1.738,1.738,0,0,0,110.4,265.022ZM12.966,322.07A12.881,12.881,0,0,1,0,309.1V265.887a12.881,12.881,0,0,1,12.966-12.966H68.958v11.621H14.983a3.465,3.465,0,0,0-2.5.912,3.219,3.219,0,0,0-.96,2.447v39.185a3.222,3.222,0,0,0,.96,2.45,3.479,3.479,0,0,0,2.5.912H68.958V322.07Zm447.59-115.062-35.187-36.281V118.252l-10.617-9.146V0H555.33L594.5,38.757V207.008Zm-17.49-54.258v10.8l24.98,25.759H576.8V46.186L571.7,41.117Zm-10.617-51.757,10.617,9.142v19.184L559.1,28.616,548.1,17.7H432.448ZM254.618,207.008l-35.187-36.281V118.252l-10.617-9.146V0h140.58l39.169,38.757V207.008ZM237.124,152.75v10.8l24.981,25.759h108.76V46.186l-5.106-5.069Zm-10.616-51.757,10.616,9.142v19.184l116.041-100.7-11-10.919H226.508ZM45.8,207.008,10.617,170.728V118.252L0,109.107V0H140.58l39.169,38.757V207.008ZM28.314,152.75v10.8l24.978,25.759h108.76V46.186l-5.106-5.069ZM17.7,100.994l10.617,9.142v19.184l116.037-100.7-11-10.919H17.7Z"
               transform="translate(0 0)" fill="#fff" />
@@ -61,7 +61,7 @@
             <div class="member-social">{{ m.job }}</div>
           </a>
         </div>
-        <a href="mailto:venture@000.capital" class="section-link">
+        <a href="https://hffyawljjws.typeform.com/to/xFpJhs98" target="_blank" class="section-link">
           <span class="text-end">Apply to Join 000</span>
           <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none" stroke="#fff"
             stroke-width="1">
@@ -192,31 +192,29 @@ export default {
     scrambleText() {
       const element = this.$refs.scrambleText;
       const originalText = element.innerText;
-      let scrambledText = '';
-      const scrambleDuration = 1000; // Duration of the effect in milliseconds
-      const scrambleIntervalTime = 30; // Time between each scramble in milliseconds
-      let elapsedTime = 0;
+      let displayedText = '';
+      const scrambleDuration = 1000; // Total duration of the effect in milliseconds
+      const letterInterval = scrambleDuration / originalText.length; // Time to reveal each letter
+
+      let index = 0;
 
       const scrambleInterval = setInterval(() => {
-        scrambledText = '';
-
-        for (let i = 0; i < originalText.length; i++) {
-          scrambledText += this.randomChar();
-        }
-
-        element.innerText = scrambledText;
-
-        elapsedTime += scrambleIntervalTime;
-        if (elapsedTime >= scrambleDuration) {
+        if (index < originalText.length) {
+          displayedText += originalText[index]; // Add the next letter from the original text
+          element.innerText = displayedText + this.randomChars(originalText.length - index - 1);
+          index++;
+        } else {
           clearInterval(scrambleInterval);
-          element.innerText = originalText; // Reset to original text after scrambling
+          element.innerText = originalText; // Display the complete original text
         }
-      }, scrambleIntervalTime);
+      }, letterInterval);
     },
-    randomChar() {
+
+    randomChars(length) {
       const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]};:\'",<.>/?';
-      return chars.charAt(Math.floor(Math.random() * chars.length));
+      return Array.from({ length }, () => chars.charAt(Math.floor(Math.random() * chars.length))).join('');
     },
+
     onScroll() {
       const sections = document.querySelectorAll('.section');
       const scrollPosition = window.scrollY + window.innerHeight / 3; // Adjusted to a third of the viewport
